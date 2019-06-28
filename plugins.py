00839bc7f23388341b76cdc904801c4682db095b
Oleg Matskiv <omatskiv@redhat.com>
2019-06-28 23:50:03 +0200

    Add Openshift4 IDP plugin and required files

--- plugins.py
+++ plugins.py
@@ -2,9 +2,12 @@
 
 import os
 from shutil import copy
+from distutils.dir_util import copy_tree
+from distutils import log
 
 pluginsKey = "SSO_PLUGINS"
 pluginsVolume = "/opt/plugins"
+themesPath = "/opt/themes"
 
 def getPlugins():
     if "SSO_PLUGINS" in os.environ:
@@ -18,8 +21,14 @@ def copyIfExists(plugin):
         print("Copying plugin: " + plugin)
         copy(pluginPath, pluginsVolume)
 
+def copyThemes():
+    log.set_verbosity(log.INFO)
+    log.set_threshold(log.INFO)
+    copy_tree('./themes', themesPath, verbose=1)
+
 def main():
     for plugin in getPlugins():
         copyIfExists(plugin)
+    copyThemes()
 
 main()
